50105f2f8c48a1a53b21a23994990da021d11a75
gonzaamat <gonzaamat@gmail.com>
2016-06-22 16:07:30 -0300

    Creacion de la clase Dependencia, DependenciaDAO, y xml de mapeo

---
+++ src/core/DependenciaDAO.java
@@ -0,0 +1,89 @@
+package core;
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+
+import entidades.Dependencia;
+import entidades.Tema;
+
+public class DependenciaDAO extends GenericDAO{
+	
+	//create
+	public int create(Dependencia dependencia){
+		 int id = -1;
+	     try {
+	    	 beginTransaction();
+	         id = (int)sesion.save(dependencia);
+	         tx.commit();
+	         endTransaction();
+	     } catch(HibernateException he) {
+	         rollbackTransaction(he);
+	         throw he;
+	     }
+	     return id;
+	}
+	
+	//read
+	public Dependencia read(int id) {
+        Dependencia dependencia = null;
+        try { 
+            beginTransaction();
+            dependencia = (Dependencia)sesion.get(Dependencia.class, id);
+            endTransaction();
+        } catch(HibernateException he) {
+            throw he;
+        }
+        System.out.println("tema : " + dependencia.getTema().getId() + " correlativo: " + dependencia.getCorrelativo().getId());
+        return dependencia; 
+    }
+	
+	//Read
+    public List<Dependencia> read() { 
+        List<Dependencia> dependencias = null;
+        try {
+            beginTransaction();
+            dependencias = sesion.createQuery("from Dependencia").list(); 
+            endTransaction();
+        } catch(HibernateException he) {
+            throw he;
+        }
+        for (Dependencia d: dependencias){
+        	System.out.println("tema : " + d.getTema().getId() + " correlativo: " + d.getCorrelativo().getId());
+        }
+        return dependencias; 
+    }
+    
+  //Update
+    public void update(Dependencia dependencia) {
+        try {
+            beginTransaction();
+            sesion.update(dependencia);
+            tx.commit();
+            endTransaction();
+        } catch(HibernateException he) {
+            rollbackTransaction(he);
+            throw he;
+        }
+    }
+    
+    //Delete
+    public void delete(Dependencia dependencia) {
+        try {
+            beginTransaction();
+            sesion.delete(dependencia);
+            tx.commit();
+            endTransaction();
+        } catch(HibernateException he) {
+            rollbackTransaction(he);
+            throw he;
+        }
+    }
+    
+	
+	
+	
+	
+}
+
+
---
+++ src/entidades/Dependencia.java
@@ -0,0 +1,42 @@
+package entidades;
+
+public class Dependencia {
+	private int id;	
+	private Tema tema; 
+	private Tema correlativo;
+	
+	public Dependencia(){
+		
+	}
+	public Dependencia(Tema tema, Tema correlativo){
+		this.tema = tema;
+		this.correlativo = correlativo;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
+	public Tema getCorrelativo() {
+		return correlativo;
+	}
+
+	public void setCorrelativo(Tema correlativo) {
+		this.correlativo = correlativo;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+
+}
